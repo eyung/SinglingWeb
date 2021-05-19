eff82a4fbac9bd6406a2e3388f00cfdaefde5db6
eyung <effiamyung@gmail.com>
2021-05-18 19:05:01 -0700

    Added Singling classes

---
+++ src/main/java/com/singling/swfrontend/SwFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.singling.swfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singling/swfrontend/SwFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singling.swfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
